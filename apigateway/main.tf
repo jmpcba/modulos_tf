47db2caccf6f5fe709bc06c3b821ae842a89320c
Manuel <jmpcba@gmail.com>
2020-04-06 22:44:12 -0300

    tf 12 sintax

--- apigateway/main.tf
+++ apigateway/main.tf
@@ -1,6 +1,6 @@
 resource "aws_api_gateway_rest_api" "apigateway_api" {
-  name = "${var.api_name}"
-  description = "${var.api_description}"
+  name = var.api_name
+  description = var.api_description
 }
 
 # STAGES
--- apigateway/outputs.tf
+++ apigateway/outputs.tf
@@ -1,15 +1,15 @@
 output "api_id" {
-  value = "${aws_api_gateway_rest_api.apigateway_api.id}"
+  value = aws_api_gateway_rest_api.apigateway_api.id
 }
 
 output "dev_url" {
-  value = "${aws_api_gateway_stage.dev_stage.invoke_url}"
+  value = aws_api_gateway_stage.dev_stage.invoke_url
 }
 
 output "prod_url" {
-  value = "${aws_api_gateway_stage.prod_stage.invoke_url}"
+  value = aws_api_gateway_stage.prod_stage.invoke_url
 }
 
-output "execution_arn"{
-  value = "${aws_api_gateway_rest_api.execution_ar}"
+output "execution_arn" {
+  value = aws_api_gateway_rest_api.execution_ar
 }
