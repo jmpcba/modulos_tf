165f0d206d7ee9acfe1854581380327c798358bc
Manuel <jmpcba@gmail.com>
2020-04-07 01:03:02 -0300

    stages and deployments

--- apigateway_resource/main.tf
+++ apigateway_resource/main.tf
@@ -3,6 +3,32 @@ resource "aws_api_gateway_rest_api" "api" {
   description = var.api_description
 }
 
+# STAGES
+resource "aws_api_gateway_stage" "prod_stage" {
+  stage_name    = "${var.client_name}_PROD"
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  deployment_id = aws_api_gateway_deployment.prod_deployment.id
+}
+
+resource "aws_api_gateway_stage" "dev_stage" {
+  stage_name    = "${var.client_name}_DEV"
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  deployment_id = aws_api_gateway_deployment.dev_deployment.id
+}
+
+resource "aws_api_gateway_deployment" "prod_deployment" {
+  depends_on  = [aws_api_gateway_integration.get_integration, aws_api_gateway_integration.post_integration, aws_api_gateway_integration.put_integration]
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = aws_api_gateway_stage.prod_stage.stage_name
+}
+
+resource "aws_api_gateway_deployment" "dev_deployment" {
+  depends_on  = [aws_api_gateway_integration.get_integration, aws_api_gateway_integration.post_integration, aws_api_gateway_integration.put_integration]
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = aws_api_gateway_stage.dev_stage.stage_name
+}
+
+# RESOURCES
 resource "aws_api_gateway_resource" "api_version_1" {
     rest_api_id = aws_api_gateway_rest_api.api.id
     parent_id   = aws_api_gateway_rest_api.api.root_resource_id
@@ -16,24 +42,13 @@ resource "aws_api_gateway_resource" "resource" {
     path_part   = element(var.resource_list, count.index)
 }
 
-
+#HTTP METHODS
 resource "aws_api_gateway_method" "put_method" {
     count                = length(var.resource_list)
     rest_api_id          = aws_api_gateway_rest_api.api.id
     resource_id          = element(aws_api_gateway_resource.resource.*.id, count.index)
     http_method          = "PUT"
     authorization        = "NONE"
-}
-
-resource "aws_api_gateway_integration" "put_integration" {
-    count                   = length(var.resource_list)
-    rest_api_id             = aws_api_gateway_rest_api.api.id
-    resource_id             = element(aws_api_gateway_resource.resource.*.id, count.index)
-    http_method             = element(aws_api_gateway_method.put_method.*.http_method, count.index)
-    content_handling        = "CONVERT_TO_TEXT" 
-    integration_http_method = "POST"
-    type                    = "AWS_PROXY"
-    uri                     = var.lambda_uri
 }
 
 resource "aws_api_gateway_method" "get_method" {
@@ -44,7 +59,16 @@ resource "aws_api_gateway_method" "get_method" {
     authorization        = "NONE"
 }
 
-resource "aws_api_gateway_integration" "prestadores_get_integration" {
+resource "aws_api_gateway_method" "post_method" {
+    count                = length(var.resource_list)
+    rest_api_id          = aws_api_gateway_rest_api.api.id
+    resource_id          = element(aws_api_gateway_resource.resource.*.id, count.index)
+    http_method          = "POST"
+    authorization        = "NONE"
+}
+
+# INTEGRATIONS
+resource "aws_api_gateway_integration" "get_integration" {
     count                   = length(var.resource_list)
     rest_api_id             = aws_api_gateway_rest_api.api.id
     resource_id             = element(aws_api_gateway_resource.resource.*.id, count.index)
@@ -55,19 +79,22 @@ resource "aws_api_gateway_integration" "prestadores_get_integration" {
     uri                     = var.lambda_uri
 }
 
-resource "aws_api_gateway_method" "post_method" {
-    count                = length(var.resource_list)
-    rest_api_id          = aws_api_gateway_rest_api.api.id
-    resource_id          = element(aws_api_gateway_resource.resource.*.id, count.index)
-    http_method          = "POST"
-    authorization        = "NONE"
-}
-
 resource "aws_api_gateway_integration" "post_integration" {
     count                   = length(var.resource_list)
     rest_api_id             = aws_api_gateway_rest_api.api.id
     resource_id             = element(aws_api_gateway_resource.resource.*.id, count.index)
     http_method             = element(aws_api_gateway_method.post_method.*.http_method, count.index)
+    content_handling        = "CONVERT_TO_TEXT" 
+    integration_http_method = "POST"
+    type                    = "AWS_PROXY"
+    uri                     = var.lambda_uri
+}
+
+resource "aws_api_gateway_integration" "put_integration" {
+    count                   = length(var.resource_list)
+    rest_api_id             = aws_api_gateway_rest_api.api.id
+    resource_id             = element(aws_api_gateway_resource.resource.*.id, count.index)
+    http_method             = element(aws_api_gateway_method.put_method.*.http_method, count.index)
     content_handling        = "CONVERT_TO_TEXT" 
     integration_http_method = "POST"
     type                    = "AWS_PROXY"
