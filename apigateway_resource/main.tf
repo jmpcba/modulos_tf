466dbc47507ae9faa3aefa9095ce8cf76ac82698
Manuel <jmpcba@gmail.com>
2020-04-07 00:34:11 -0300

    remove varaiables

--- apigateway_resource/main.tf
+++ apigateway_resource/main.tf
@@ -1,12 +1,17 @@
+resource "aws_api_gateway_rest_api" "api" {
+  name = var.api_name
+  description = var.api_description
+}
+
 resource "aws_api_gateway_resource" "api_version_1" {
-    rest_api_id = var.restapi_id
-    parent_id   = var.root_resource_id
+    rest_api_id = aws_api_gateway_rest_api.api.id
+    parent_id   = aws_api_gateway_rest_api.api.root_resource_id
     path_part   = "v1"
 }
 
 resource "aws_api_gateway_resource" "resource" {
     count       = length(var.resource_list)
-    rest_api_id = var.restapi_id
+    rest_api_id = aws_api_gateway_rest_api.api.id
     parent_id   = aws_api_gateway_resource.api_version_1.id
     path_part   = element(var.resource_list, count.index)
 }
@@ -14,7 +19,7 @@ resource "aws_api_gateway_resource" "resource" {
 
 resource "aws_api_gateway_method" "put_method" {
     count                = length(var.resource_list)
-    rest_api_id          = var.restapi_id
+    rest_api_id          = aws_api_gateway_rest_api.api.id
     resource_id          = element(aws_api_gateway_resource.resource.*.id, count.index)
     http_method          = "PUT"
     authorization        = "NONE"
@@ -22,7 +27,7 @@ resource "aws_api_gateway_method" "put_method" {
 
 resource "aws_api_gateway_integration" "put_integration" {
     count                   = length(var.resource_list)
-    rest_api_id             = var.restapi_id
+    rest_api_id             = aws_api_gateway_rest_api.api.id
     resource_id             = element(aws_api_gateway_resource.resource.*.id, count.index)
     http_method             = aws_api_gateway_method.put_method.http_method
     content_handling        = "CONVERT_TO_TEXT" 
@@ -33,7 +38,7 @@ resource "aws_api_gateway_integration" "put_integration" {
 
 resource "aws_api_gateway_method" "get_method" {
     count                = length(var.resource_list)
-    rest_api_id          = var.restapi_id
+    rest_api_id          = aws_api_gateway_rest_api.api.id
     resource_id          = element(aws_api_gateway_resource.resource.*.id, count.index)
     http_method          = "GET"
     authorization        = "NONE"
@@ -41,7 +46,7 @@ resource "aws_api_gateway_method" "get_method" {
 
 resource "aws_api_gateway_integration" "prestadores_get_integration" {
     count                   = length(var.resource_list)
-    rest_api_id             = var.restapi_id
+    rest_api_id             = aws_api_gateway_rest_api.api.id
     resource_id             = element(aws_api_gateway_resource.resource.*.id, count.index)
     http_method             = aws_api_gateway_method.get_method.http_method
     content_handling        = "CONVERT_TO_TEXT" 
@@ -52,7 +57,7 @@ resource "aws_api_gateway_integration" "prestadores_get_integration" {
 
 resource "aws_api_gateway_method" "post_method" {
     count                = length(var.resource_list)
-    rest_api_id          = var.restapi_id
+    rest_api_id          = aws_api_gateway_rest_api.api.id
     resource_id          = element(aws_api_gateway_resource.resource.*.id, count.index)
     http_method          = "POST"
     authorization        = "NONE"
@@ -60,7 +65,7 @@ resource "aws_api_gateway_method" "post_method" {
 
 resource "aws_api_gateway_integration" "post_integration" {
     count                   = length(var.resource_list)
-    rest_api_id             = var.restapi_id
+    rest_api_id             = aws_api_gateway_rest_api.api.id
     resource_id             = element(aws_api_gateway_resource.resource.*.id, count.index)
     http_method             = aws_api_gateway_method.post_method.http_method
     content_handling        = "CONVERT_TO_TEXT" 
--- apigateway_resource/variables.tf
+++ apigateway_resource/variables.tf
@@ -1,10 +1,4 @@
-variable "restapi_id" {}
-
-variable "resource_path" {}
-
 variable "lambda_uri" {}
-
-variable "root_resource_id" {}
 
 variable "resource_list" {
     type = list
