72b6070cb18f2394f079f54d2acd351939d2d5c8
Manuel <jmpcba@gmail.com>
2020-04-06 23:10:36 -0300

    add depends_on

--- apigateway/main.tf
+++ apigateway/main.tf
@@ -17,11 +17,13 @@ resource "aws_api_gateway_stage" "dev_stage" {
 }
 
 resource "aws_api_gateway_deployment" "prod_deployment" {
+  depends_on  = aws_api_gateway_stage.prod_stage
   rest_api_id = aws_api_gateway_rest_api.apigateway_api.id
   stage_name  = aws_api_gateway_stage.prod_stage.stage_name
 }
 
 resource "aws_api_gateway_deployment" "dev_deployment" {
+  depends_on  = aws_api_gateway_stage.dev_stage
   rest_api_id = aws_api_gateway_rest_api.apigateway_api.id
   stage_name  = aws_api_gateway_stage.dev_stage.stage_name
  }
