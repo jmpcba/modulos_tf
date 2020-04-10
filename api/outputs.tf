da4c82889c20086bb97e5f1000c0cc252886a0a9
jupalacios <jupalacios@contractor.ea.com>
2020-04-10 01:48:35 -0300

    replace stage invoke url output with deployment url

--- api/outputs.tf
+++ api/outputs.tf
@@ -11,9 +11,9 @@ output "root_resource_id" {
 }
 
 output "dev_url" {
-  value = aws_api_gateway_stage.dev_stage.invoke_url
+  value = aws_api_gateway_deployment.dev_deployment.invoke_url
 }
 
 output "prod_url" {
-  value = aws_api_gateway_stage.prod_stage.invoke_url
+  value = aws_api_gateway_deployment.prod_deployment.invoke_url
 }
