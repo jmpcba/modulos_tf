fa7af84f7180c12c81499822bbe381bea17c4e66
jupalacios <jupalacios@contractor.ea.com>
2020-04-10 01:00:01 -0300

    remove optional deployment stage name parameter

--- api/main.tf
+++ api/main.tf
@@ -16,16 +16,15 @@ resource "aws_api_gateway_stage" "dev_stage" {
   deployment_id = aws_api_gateway_deployment.dev_deployment.id
 }
 
+# DEPLOYMENTS
 resource "aws_api_gateway_deployment" "prod_deployment" {
   depends_on  = [aws_api_gateway_integration.get_integration, aws_api_gateway_integration.post_integration, aws_api_gateway_integration.put_integration]
   rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name  = "prod"
 }
 
 resource "aws_api_gateway_deployment" "dev_deployment" {
   depends_on  = [aws_api_gateway_integration.get_integration, aws_api_gateway_integration.post_integration, aws_api_gateway_integration.put_integration]
   rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name  = "dev"
 }
 
 # RESOURCES
