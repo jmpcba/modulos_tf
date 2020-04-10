168789d2d8d805e5ad2ec0816a42d02f9e2da616
jupalacios <jupalacios@contractor.ea.com>
2020-04-10 01:20:24 -0300

    re enable stages and deployments

--- api/main.tf
+++ api/main.tf
@@ -20,11 +20,13 @@ resource "aws_api_gateway_stage" "dev_stage" {
 resource "aws_api_gateway_deployment" "prod_deployment" {
   depends_on  = [aws_api_gateway_integration.get_integration, aws_api_gateway_integration.post_integration, aws_api_gateway_integration.put_integration]
   rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name    = "prod"
 }
 
 resource "aws_api_gateway_deployment" "dev_deployment" {
   depends_on  = [aws_api_gateway_integration.get_integration, aws_api_gateway_integration.post_integration, aws_api_gateway_integration.put_integration]
   rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name    = "dev"
 }
 
 # RESOURCES
