2efb05eaecb8f50c2881a81d5eaeed7c4796d7e6
jupalacios <jupalacios@contractor.ea.com>
2020-04-10 01:06:45 -0300

    remove deployments resources

--- api/main.tf
+++ api/main.tf
@@ -16,17 +16,6 @@ resource "aws_api_gateway_stage" "dev_stage" {
   deployment_id = aws_api_gateway_deployment.dev_deployment.id
 }
 
-# DEPLOYMENTS
-resource "aws_api_gateway_deployment" "prod_deployment" {
-  depends_on  = [aws_api_gateway_integration.get_integration, aws_api_gateway_integration.post_integration, aws_api_gateway_integration.put_integration]
-  rest_api_id = aws_api_gateway_rest_api.api.id
-}
-
-resource "aws_api_gateway_deployment" "dev_deployment" {
-  depends_on  = [aws_api_gateway_integration.get_integration, aws_api_gateway_integration.post_integration, aws_api_gateway_integration.put_integration]
-  rest_api_id = aws_api_gateway_rest_api.api.id
-}
-
 # RESOURCES
 resource "aws_api_gateway_resource" "api_version_1" {
     rest_api_id = aws_api_gateway_rest_api.api.id
