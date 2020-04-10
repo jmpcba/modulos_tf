77046b5ea5f0b00f196b808e1e1dc10eb23cfb55
jupalacios <jupalacios@contractor.ea.com>
2020-04-10 00:45:39 -0300

    naming conflict in prod dpeloyment

--- api/main.tf
+++ api/main.tf
@@ -19,7 +19,7 @@ resource "aws_api_gateway_stage" "dev_stage" {
 resource "aws_api_gateway_deployment" "prod_deployment" {
   depends_on  = [aws_api_gateway_integration.get_integration, aws_api_gateway_integration.post_integration, aws_api_gateway_integration.put_integration]
   rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name  = "dev"
+  stage_name  = "prod"
 }
 
 resource "aws_api_gateway_deployment" "dev_deployment" {
