2dfba3b9347ab2a8e0dfe4fa4eb08718c1acbdc1
Manuel <jmpcba@gmail.com>
2020-04-17 22:31:11 -0300

    add header to cognito authorizer

--- api/main.tf
+++ api/main.tf
@@ -100,6 +100,7 @@ resource "aws_api_gateway_authorizer" "cognito_authorizer" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   type          = "COGNITO_USER_POOLS"
   provider_arns = aws_cognito_user_pool.user_pool.arn
+  identity_source = "method.request.header.X-COG-ID"
 }
 
 resource "aws_cognito_user_pool" "user_pool" {
