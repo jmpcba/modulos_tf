7e9ad6f14c43ff653fe46a9b15721c73f7c9e7df
Manuel <jmpcba@gmail.com>
2020-04-17 23:06:24 -0300

    sintax error

--- api/main.tf
+++ api/main.tf
@@ -96,7 +96,7 @@ resource "aws_api_gateway_authorizer" "cognito_authorizer" {
   name          = "cognito_authorizer"
   rest_api_id   = aws_api_gateway_rest_api.api.id
   type          = "COGNITO_USER_POOLS"
-  provider_arns = [aws_cognito_user_pool.user_pool.arns,]
+  provider_arns = [aws_cognito_user_pool.user_pool.arn,]
   identity_source = "method.request.header.X-COG-ID"
 }
 
